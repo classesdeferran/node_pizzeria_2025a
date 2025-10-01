46af3d3f5e4948c0e911dc5ce0842c0fb7ff1f9c
Ferran <ferran.docencia@gmail.com>
2025-10-01 08:35:14 +0200

    Pizzeria

---
+++ ingredientes.js
@@ -0,0 +1,46 @@
+const ingredientes = {
+  "A": {
+    "nombre": "Masa normal",
+    "precio": 3
+  },
+  "B": {
+    "nombre": "Masa espelta",
+    "precio": 3.5
+  },
+  "C": {
+    "nombre": "Masa sin gluten",
+    "precio": 3.5
+  },
+  "1" : {
+    "nombre": "tomate",
+    "precio": 1.5
+  },
+  "2" : {
+    "nombre": "mozzarella",
+    "precio": 2.8
+  },
+    "3" : {
+    "nombre": "champiñones",
+    "precio": 2.5
+  },
+  "4" : {
+    "nombre": "gorgonzola",
+    "precio": 3.8
+  },
+    "5" : {
+    "nombre": "parmesano",
+    "precio": 3.5
+  },
+  "6" : {
+    "nombre": "bacon",
+    "precio": 3.4
+  },
+    "7" : {
+    "nombre": "atún",
+    "precio": 4.5
+  },
+  "8" : {
+    "nombre": "nduja",
+    "precio": 3.8
+  }
+};
---
+++ pizzeria.js
@@ -0,0 +1,29 @@
+// ENUNCIADO
+
+// 1 - Mostrar el menú con las opciones:
+
+// Menú de la pizzeria
+// ===================
+
+// A. Masa normal
+// B. Masa espelta
+// ....
+// 1. tomate
+// ...
+
+// Al ejecutar el fichero el usuario hace la elección
+// de los ingredientes, pero hay que respetar estas 
+// condiciones:
+
+// 1) Tiene que elegir un tipo de masa, pero solo uno
+// 2) Tiene que elegir como mínimo tres ingredientes y
+// como máximo cinco. Si elige menos, mostraremos 
+// un mensaje de error, si eleige más de 5, sólo 
+// utilizaremos los cinco primeros.
+
+// Ejemplo:
+// node ./pizzeria.js C 1 3 5 6
+
+// Con esto, indicaremos al usuario que ha elegido y cuanto cuesta.
+// "Preparamos tu pizza con masa sin gluten, tomate, champiñones, parmesano, bacon."
+// "Precio: 14.40€"
